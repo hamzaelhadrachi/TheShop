86a56b4f516f7dbde3e0819077f7cf8849596b43
Hamza <hamza.elhadrachi@gmail.com>
2022-05-04 14:25:09 +0000

    initial project creation

---
+++ ShopWebParent/ShopBackEnd/src/main/java/com/zerotohero/admin/ShopbackendApplication.java
@@ -0,0 +1,13 @@
+package com.zerotohero.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopbackendApplication.class, args);
+	}
+
+}
---
+++ ShopWebParent/ShopFrontEnd/src/main/java/com/zerotohero/site/ShopfrontendApplication.java
@@ -0,0 +1,13 @@
+package com.zerotohero.site;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopfrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopfrontendApplication.class, args);
+	}
+
+}
