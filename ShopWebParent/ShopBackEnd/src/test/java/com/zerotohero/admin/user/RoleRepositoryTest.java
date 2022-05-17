9a6f6c571c5c4704c39b66619bdd07b870d38caa
Hamza <hamza.elhadrachi@gmail.com>
2022-05-17 12:32:24 +0100

    enhancing unit test for role repository

--- ShopWebParent/ShopBackEnd/src/test/java/com/zerotohero/admin/user/RoleRepositoryTest.java
+++ ShopWebParent/ShopBackEnd/src/test/java/com/zerotohero/admin/user/RoleRepositoryTest.java
@@ -5,6 +5,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.Arrays;
+import java.util.function.Consumer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -14,18 +18,25 @@ import static org.assertj.core.api.Assertions.assertThat;
  */
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
 class RoleRepositoryTest {
 
     @Autowired
     RoleRepository repository;
 
     @Test
-    public void testCreateFirstRole(){
+    public void testCreateRestOfRoles(){
 
         Role adminRole  = new Role("Admin", "Manages Every Thing");
-        Role savedRole = repository.save(adminRole);
+        Role salesPersonRole  = new Role("SalesPerson", "manages ProductPrice, Customers, Shipping, Orders and Sales Reports.");
+        Role editorRole  = new Role("Editor", "manages Categories, Brands, Products, Articles and Menus.");
+        Role shipperRole  = new Role("Shipper", "view products, view orders and update order status.");
+        Role assistantRole  = new Role("Assistant", "manages questions and reviews.");
+        Iterable<Role> savedRoles = repository.saveAll(Arrays.asList(adminRole, salesPersonRole, editorRole, shipperRole, assistantRole));
 
-        assertThat(savedRole.getId()).isGreaterThan(0);
+        savedRoles.forEach(
+                role -> assertThat(role.getId()).isGreaterThan(0)
+        );
     }
 
 }
