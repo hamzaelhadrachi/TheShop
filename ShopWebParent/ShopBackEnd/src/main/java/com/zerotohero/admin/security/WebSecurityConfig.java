9e4f06e7b4734d6a8541e147025d3f0482010519
HAMZAELHADRACHI <hamza.elhadrachi@gmail.com>
2023-02-20 11:00:01 +0100

    initial commit for user authentication

--- ShopWebParent/ShopBackEnd/src/main/java/com/zerotohero/admin/security/WebSecurityConfig.java
+++ ShopWebParent/ShopBackEnd/src/main/java/com/zerotohero/admin/security/WebSecurityConfig.java
@@ -34,6 +34,5 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	public void configure(WebSecurity web) throws Exception {
 		web.ignoring().antMatchers("/images/**","/js/**","/webjars/**");
 	}
-    
-    
+
 }
